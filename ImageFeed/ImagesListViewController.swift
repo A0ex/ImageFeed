db0d97b520cc345b434255e6ac44ef5c1241bf11
A0ex <alxslvrstv@yandex.ru>
2024-03-17 13:19:57 +0300

    Добавлен ImageView с таблицей

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ImagesListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet private var tableView: UITableView!
+    
 }
 
